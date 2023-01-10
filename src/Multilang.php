a79f01aa73333019afa45928db6b66fd2c07e7e6
farouqzakwan <69852327+rndmomentum@users.noreply.github.com>
2023-01-10 11:42:04 +0800

    fix

--- src/Multilang.php
+++ src/Multilang.php
@@ -20,19 +20,19 @@ class Multilang
         $explode = explode('.',$file);
         if(file_exists($this->directory.'/'.$this->language.'/'.$explode[0].'.json'))
         {
-            $lang_file = file_get_contents($this->directory.'/'.$this->language, $explode[0].'.json');
+            $lang_file = file_get_contents(__DIR__.'/'.$this->directory.'/'.$this->language, $explode[0].'.json');
 
             $a = json_decode($lang_file,true);
             return $a[$explode[1]];
         }
 
-        if(file_exists($this->directory.'/'.Config::LANG.'/'.$explode[0].'.json'))
-        {
-            $lang_file = file_get_contents($this->directory.'/'.$this->language, $explode[0].'.json');
+        // if(file_exists($this->directory.'/'.Config::LANG.'/'.$explode[0].'.json'))
+        // {
+        //     $lang_file = file_get_contents($this->directory.'/'.$this->language, $explode[0].'.json');
 
-            $a = json_decode($lang_file,true);
-            return $a[$explode[1]];
-        }
+        //     $a = json_decode($lang_file,true);
+        //     return $a[$explode[1]];
+        // }
 
         return $file;
     }
